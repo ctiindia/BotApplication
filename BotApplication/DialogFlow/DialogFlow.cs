2a5d214d010e2f2b1ded3a69c59bdfedadf2892d
ctiindia <43128513+ctiindia@users.noreply.github.com>
2018-09-10 11:54:04 +0530

    test bot application
    test bot app

---
+++ BotApplication/DialogFlow/DialogFlow.cs
@@ -0,0 +1,40 @@
+﻿using ApiAiSDK;
+using ApiAiSDK.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BotApplication.Integrate
+{
+    public static class DialogFlow
+    {
+
+        public static string accessToken = "89c165e158f2400cbe369888333e6381";
+
+        /// <summary>
+        /// connecting to the dialogflow
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        public static string getDialogfloW(Request request)
+        {
+            var config = new AIConfiguration(accessToken, SupportedLanguage.English);
+            
+            var dataService = new AIDataService(config);
+            var aiRequest = new AIRequest(request.Message);
+            var aiResponse = dataService.Request(aiRequest);
+
+            var dialogflowresult = aiResponse.Result.Fulfillment.Speech;
+
+            return dialogflowresult;
+        }
+
+    }
+}
+
+public class Request
+{
+    public string SessionId { get; set; }
+    public string Message { get; set; }
+}
---
+++ BotApplication/Dialogs/RootDialog.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BotApplication.Integrate;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace BotApplication.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;
+
+            // Calculate something for us to return
+            int length = (activity.Text ?? string.Empty).Length;
+            if(length > 0)
+            {
+                Request request = new Request();
+                request.Message = activity.Text.ToString();
+                request.SessionId = "";
+                var response = DialogFlow.getDialogfloW(request);
+        
+                // Return our reply to the user
+                await context.PostAsync(response);
+
+                context.Wait(MessageReceivedAsync);
+                return;
+            }
+            // Return our reply to the user
+            await context.PostAsync($"Invalid request");
+
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
+
